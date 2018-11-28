e22a93561a14d31db524da6a4b2b293120408560
Jimmy419 <suifeng419@126.com>
2018-11-28 21:56:34 +0800

    third commit

--- src/component/Records.js
+++ src/component/Records.js
@@ -25,6 +25,7 @@ export default class Records extends Component{
         }
         console.log('first commit');
         console.log('second commit');
+        console.log('third commit');
     }
 
     componentDidMount(){
